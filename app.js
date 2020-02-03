f53421efd83930c6b4a3521e3b87eb94b66c9cd9
sunilnagargojecc <59916458+sunilnagargojecc@users.noreply.github.com>
2020-02-03 16:28:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,12 +10,27 @@ var request     = require('request');
 var routes      = require('./routes');
 var activity    = require('./routes/activity');
 
+/*let Client = require('ssh2-sftp-client');
+let sftp = new Client();*/
+
+
+
+console.log('in app.js file');
+
 var app = express();
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({type: 'application/jwt'}));
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/example', (req, res) => {
+ res.send(req.body.filename + req.body.sourcename + req.body.sendername + req.body.msg);
+ 
+	
+ console.log('POST FORM BODY', req.body);
+});
+
 
 //app.use(express.methodOverride());
 //app.use(express.favicon());
@@ -37,6 +52,7 @@ app.post('/journeybuilder/save/', activity.save );
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
