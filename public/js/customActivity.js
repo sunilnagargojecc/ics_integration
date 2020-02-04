64e4689b4d02940060ed8e0ec79bd58f379d14d9
sunilnagargojecc <59916458+sunilnagargojecc@users.noreply.github.com>
2020-02-04 20:04:57 +0530

    Add files via upload

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,135 +1,84 @@
-define( function( require ) {
+define([
+    'postmonger'
+], function (
+    Postmonger
+) {
     'use strict';
-    
-	var Postmonger = require('postmonger');
-	var $ = require('jquery');
 
-  	var connection = new Postmonger.Session();
-    var toJbPayload = {};
-    var step = 1; 
-	var tokens;
-	var endpoints;
-	
+    var connection = new Postmonger.Session();
+    var authTokens = {};
+    var payload = {};
     $(window).ready(onRender);
 
-    connection.on('initActivity', function(payload) {
-        var eventDefinitionKey;
-        var webhookUrl;
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
 
-        if (payload) {
-            toJbPayload = payload;
-            console.log('payload',payload);
-            
-            eventDefinitionKey = toJbPayload['arguments'].execute.inArguments[0].eventDefinitionKey;
-            webhookUrl = toJbPayload['arguments'].execute.inArguments[0].webhookUrl;
-            
-            $('#txtWebhookUrl').val(webhookUrl);         
-        }
-      
-        // setup availabe events
-		$.get( "/zapier/eventdefinitions", function( data ) {
-		    $.each(data, function(i,item) {
-                $("#selEventDefinitionKey").append("<option value="+item.eventDefinitionKey+">"+item.name+"</option>");
-            });
-            $('#selEventDefinitionKey').find('option[value='+ eventDefinitionKey +']').attr('selected', 'selected');	         
-		});
-      	         
-        // set state of the form
-        if (!eventDefinitionKey) {
-            connection.trigger('updateButton', { button: 'next', enabled: false });
-        }
-        if (!webhookUrl) {
-            connection.trigger('updateButton', { button: 'next', enabled: false });
-        }	
-		gotoStep(step);
-        
-    });
-
-    connection.on('requestedTokens', function(data) {
-		if( data.error ) {
-			console.error( data.error );
-		} else {
-            console.log(data);
-			tokens = data;
-		}        
-    });
-
-    connection.on('requestedEndpoints', function(data) {
-		if( data.error ) {
-			console.error( data.error );
-		} else {
-            console.log(data);
-			endpoints = data;
-		}        
-    });
-
-    connection.on('clickedNext', function() {
-        step++;
-        gotoStep(step);
-        connection.trigger('ready');
-    });
-
-    connection.on('clickedBack', function() {
-        step--;
-        gotoStep(step);
-        connection.trigger('ready');
-    });
-
+    connection.on('clickedNext', save);
+   
     function onRender() {
-		console.log('onRender');
-		
+        // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
+
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
 
-        // Disable the next button if we don't have valid values
-        $('#txtWebhookUrl').on('input',function(e) {
-            connection.trigger('updateButton', { button: 'next', enabled: validValues() });
-        });
-        $('#selEventDefinitionKey').change(function() {
-            connection.trigger('updateButton', { button: 'next', enabled: validValues() });
-        });
-    };
+    }
 
-    function gotoStep(step) {
-        $('.step').hide();
-        switch(step) {
-            case 1:
-                $('#step1').show();
-                connection.trigger('updateButton', { button: 'next', text: 'done', enabled: validValues() });
-                connection.trigger('updateButton', { button: 'back', visible: false });
-                break;  
-            case 2:
-                save();
-                break;
+    function initialize(data) {
+        console.log(data);
+        if (data) {
+            payload = data;
         }
-    };
+        
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
 
-    function getEventDefinitionKey() {
-        return $('#selEventDefinitionKey').find('option:selected').attr('value').trim();
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+
+        console.log(inArguments);
+
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+                
+              
+            });
+        });
+
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
     }
 
-    function getWebhookUrl() {
-        return $.trim($("#txtWebhookUrl").val())
+    function onGetTokens(tokens) {
+        console.log(tokens);
+        authTokens = tokens;
     }
-    
-    function validValues() {
-        return getEventDefinitionKey() != "select" && getWebhookUrl().length > 0;
+
+    function onGetEndpoints(endpoints) {
+        console.log(endpoints);
     }
 
     function save() {
-        var eventDefinitionKey = getEventDefinitionKey();
-        var customObjectKey = "{{Event." + eventDefinitionKey + "._CustomObjectKey}}";
-        var webhookUrl = getWebhookUrl();
+        var postcardURLValue = $('#postcard-url').val();
+        var postcardTextValue = $('#postcard-text').val();
+
+        payload['arguments'].execute.inArguments = [{
+            "tokens": authTokens,
+            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+        }];
         
-        toJbPayload['arguments'].execute.inArguments[0].eventDefinitionKey = eventDefinitionKey;
-        toJbPayload['arguments'].execute.inArguments[0].customObjectKey = customObjectKey;
-        toJbPayload['arguments'].execute.inArguments[0].webhookUrl = webhookUrl;
-		
-        //this is required by JB to set the activity as Configured.
-		toJbPayload.metaData.isConfigured = true;  
-        connection.trigger('updateActivity', toJbPayload);
-    }; 
-    	 
+        payload['metaData'].isConfigured = true;
+
+        console.log(payload);
+        connection.trigger('updateActivity', payload);
+    }
+
+
 });
-			
